3e4e57598a3fe1de048af209beb65346d834b93e
Tommolo <72791771+Tommolo@users.noreply.github.com>
2020-11-13 19:58:35 +0100

    Script e sprite

---
+++ YoutubeProject/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    int score = 0;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Avvenuta Collisone");
+        Destroy(gameObject);
+        score++;
+        Debug.Log("Score: " + score);
+    }
+}
