778b29f3f8880e0789340a5edef653c16bd4e88e
Tommolo <72791771+Tommolo@users.noreply.github.com>
2020-11-15 17:50:54 +0100

    Jump

--- YoutubeProject/Assets/Scripts/PlayerControl.cs
+++ YoutubeProject/Assets/Scripts/PlayerControl.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 10f;
+    public float jumpSpeed = 5f;
     public float movement = 0;
     public Rigidbody2D rigidbody2d;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
@@ -16,6 +19,7 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
         movement = Input.GetAxis("Horizontal");
         if (movement < 0)
         {
@@ -29,7 +33,10 @@ public class PlayerControl : MonoBehaviour
         {
             rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
         }
-    
-    
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x,jumpSpeed);
+        }
+
     }
 }
